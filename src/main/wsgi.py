c610e8528a7ac5d1b995615736c754e6bfa5cdaa
Your Name <you@example.com>
2021-01-11 20:21:51 +0300

    print environ8

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -26,31 +26,36 @@ def application(environ, start_response):
     headers = {
         "Content-type": "text/html",
     }
+    environ2 = " "
+    for key, value in environ.items():
+        text = f"<p> {key} + {value} </p>"
+        environ2 += text
 
     payload = (
-        b"<!DOCTYPE html>"
-        b"<html>"
-        b"<head>"
-        b"<title>Alpha</title>"
-        b'<meta charset="utf-8">'
-        b"</head>"
-        b"<body>"
-        b"<h1>Project Alpha</h1>"
-        b"<hr>"
-        b"<p>This is a template project.NEW.</p>"
-        b"</body>"
-        b"</html>"
+        "<!DOCTYPE html>"
+        "<html>"
+        "<head>"
+        "<title>Alpha</title>"
+        '<meta charset="utf-8">'
+        "</head>"
+        "<body>"
+        "<h1>Project Alpha</h1>"
+        "<hr>"
+        f"{environ2}"
+        "<p>This is a template project.NEW.</p>"
+        "</body>"
+        "</html>"
     )
     # print(payload)
     # print(type(payload))
-    f = payload.decode(encoding='utf-8')
+    ##f = payload.decode(encoding='utf-8')
     # print(type(f))
-    h = f.replace('<p>This is a template project.NEW.</p>', '<p>This is a template project.NEW.{}</p>').format(x)
+    ##h = f.replace('<p>This is a template project.NEW.</p>', '<p>This is a template project.NEW.{}</p>').format(x)
     # print(h)
-    b = h.encode(encoding='utf-8')
+    ##b = h.encode(encoding='utf-8')
     # print(b)
-    payload = b
+    ##payload = b
 
     start_response(status, list(headers.items()))
 
-    yield payload
+    yield [payload.encode()]
