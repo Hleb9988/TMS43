848ba206a6e723d35c751e93c03094941e65c1e9
Your Name <you@example.com>
2021-01-27 13:44:35 +0300

    def 11

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -8,12 +8,12 @@ from framework.util.settings import get_setting
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
 
-random_number = random.randint(-100, 100)
-
-environ2 = ""
-for key, value in environ.items():
-    text = f"<tr><td>{key}</td><td>{value}</td></tr>"
-    environ2 = environ2 + text
+# random_number = random.randint(-100, 100)
+#
+# environ2 = ""
+# for key, value in environ.items():
+#     text = f"<tr><td>{key}</td><td>{value}</td></tr>"
+#     environ2 = environ2 + text
 
 
 def wrong():
@@ -22,15 +22,15 @@ def wrong():
 def index():
     t = read_template("index.html")
     payload = t.format(
-        random_number=random_number,
-        environ=environ2,
+        random_number=22,
+        environ='pjpoj',
     )
     return payload
 
 def about():
     t = read_template("environ.html")
     payload = t.format(
-        environ=environ2
+        environ='plpk0'
     )
     return payload
 
@@ -56,7 +56,7 @@ def application(environ, start_response):
     path = environ["PATH_INFO"]
     head = {
         '/e/': 'wrong',
-        '/a/': 'index',
+        '/': 'index',
         '/b': 'about'
     }
 
@@ -75,7 +75,7 @@ def application(environ, start_response):
     #     environ=environ2,
     # )
 
-    h = head[path]
+    h = head['/']
     h()
 
     start_response(status, list(headers.items()))
