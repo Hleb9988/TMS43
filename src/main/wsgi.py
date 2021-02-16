9d20ec0181803f9bb13a957b9c01ff8e35b19386
Your Name <you@example.com>
2021-02-16 13:48:37 +0300

    add home code Alex

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,150 +1,24 @@
-from typing import Callable
-from typing import Tuple
-from urllib.parse import parse_qs
-
 import sentry_sdk
 
-from framework.dirs import DIR_SRC
 from framework.util.settings import get_setting
-from tasks.lesson03 import task303
-from tasks.lesson03 import task311
-from tasks.lesson03 import task310
-
+from main.custom_types import RequestT
+from main.handlers import get_handler
+from main.handlers import handle_500
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
 
-ResponseT = Tuple[str, str, str]
-
-
-def handle_error(method: str, path: str, qs: str) -> ResponseT:
-    payload = str(1 / 0)
-    return "500 Internal Server Error", "text/plain", payload
-
-
-def handle_404(method: str, path: str, qs: str) -> ResponseT:
-    status = "404 Not Found"
-    content_type = "text/plain"
-    payload = f"OOPS! endpoint {path} not found!"
-    return status, content_type, payload
-
-
-def handle_task_303(method: str, path: str, qs: str) -> ResponseT:
-    status = "200 OK"
-    content_type = "text/html"
-
-    qsi = parse_qs(qs)
-
-    template = read_template("task_303.html")
-
-    sentence = qsi.get("sentence")
-
-    if not sentence:
-        result = ""
-    else:
-        sentence = sentence[0]
-        result = task303.solution(sentence)
-
-    payload = template.format(text=result)
-
-    return status, content_type, payload
-
-
-def handle_index(method: str, path: str, qs: str) -> ResponseT:
-    status = "200 OK"
-
-    content_type = "text/html"
-
-    template = read_template("index.html")
-
-    payload = template.format(
-        random_number=123,
-        environ={},
-    )
-
-    return status, content_type, payload
-
-
-def handle_task_311(method: str, path: str, qs: str):
-    status = "200 OK"
-    content_type = "text/html"
-
-    qsi = parse_qs(qs)
-
-    template = read_template("task_311.html")
-
-    sentence = qsi.get("sentence")
-
-    if not sentence:
-        result = ""
-    else:
-        sentence = sentence[0]
-        result = task311.is_gmail(sentence)
-
-    payload = template.format(text=result)
-
-    return status, content_type, payload
-
-
-def handle_task_310(method: str, path: str, qs: str):
-    status = "200 OK"
-    content_type = "text/html"
-
-    qsi = parse_qs(qs)
-
-    template = read_template("task_310.html")
-
-    sentence = qsi.get("sentence")
-
-    if not sentence:
-        result = ""
-    else:
-        sentence = sentence[0]
-        result = task310.nominal(sentence)
-
-    payload = template.format(text=result)
-
-    return status, content_type, payload
-
-HANDLERS = {
-    "/": handle_index,
-    "/e/": handle_error,
-    "/tasks/lesson03/task303/": handle_task_303,
-    "/tasks/lesson03/task311/": handle_task_311,
-    "/tasks/lesson03/task310/": handle_task_310,
-}
-
-
-def get_handler(path: str) -> Callable:
-    handler = HANDLERS.get(path, handle_404)
-
-    return handler
-
 
 def application(environ, start_response):
-    method = environ["REQUEST_METHOD"]
-    path = environ["PATH_INFO"]
-    query_string = environ["QUERY_STRING"]
+    request = RequestT(environ)
+    handler = get_handler(request)
 
-    handler = get_handler(path)
+    try:
+        response = handler(request)
+    except Exception:
+        response = handle_500(request)
 
-    status, content_type, payload = handler(method, path, query_string)
+    status = f"{response.status.value} {response.status.phrase}"
+    headers_list = list(response.headers_items())
+    start_response(status, headers_list)
 
-    headers = {
-        "Content-type": content_type,
-    }
-
-    start_response(status, list(headers.items()))
-
-    yield payload.encode()
-
-
-def read_template(template_name: str) -> str:
-    dir_templates = DIR_SRC / "main" / "templates"
-    template = dir_templates / template_name
-
-    assert template.is_file()
-
-    with template.open("r") as fd:
-        content = fd.read()
-
-    return content
+    yield response.payload.encode()
